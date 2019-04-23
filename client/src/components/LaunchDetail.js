9852cd4269f9a37b0859d4f8bf38dac6c9d9def7
Dániel Agg <daniel.agg@outlook.com>
2019-04-23 13:37:46 +0200

    Aligning schema, filling LaunchDetails component with data

--- client/src/components/LaunchDetail.js
+++ client/src/components/LaunchDetail.js
@@ -1,11 +1,51 @@
 import React, { Component } from "react";
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
+import { Link } from "react-router-dom";
+
+const LAUNCH_QUERY = gql`
+  query LaunchQuery($flight_number: Int!) {
+    launch(flight_number: $flight_number) {
+      flight_number
+      mission_name
+      launch_success
+      launch_date_local
+      rocket {
+        rocket_id
+        rocket_name
+        rocket_type
+      }
+    }
+  }
+`;
 
 export class LaunchDetail extends Component {
   render() {
+    let { flight_number } = this.props.match.params;
+    flight_number = parseInt(flight_number);
     return (
-      <div>
-        <h1>todo</h1>
-      </div>
+      <>
+        <Query query={LAUNCH_QUERY} variables={{ flight_number }}>
+          {({ loading, error, data }) => {
+            if (loading) return <h4>Loading...</h4>;
+            if (error) console.log(error);
+
+            const {
+              mission_name,
+              launch_date_local,
+              launch_success,
+              rocket: { rocket_id, rocket_name, rocket_type }
+            } = data.launch;
+
+            return (
+              <>
+                <h1>{mission_name}</h1>
+                <Link to="/">Back</Link>
+              </>
+            );
+          }}
+        </Query>
+      </>
     );
   }
 }
